28c2dd7e78fcedf54e31df7341d743069ebd1629
TK-open-public <itaebread2@gmail.com>
2026-01-06 04:26:11 +0900

    Feat: appendDamage 메소드 thread-safe 하도록 수정

--- src/main/kotlin/DataStorage.kt
+++ src/main/kotlin/DataStorage.kt
@@ -1,17 +1,23 @@
 package com.tbread
 
+import java.util.concurrent.ConcurrentHashMap
+
 class DataStorage {
-    private val damageStorage = HashMap<String, HashMap<String, MutableSet<ParsedDamagePacket>>>()
-    private val nicknameStorage = HashMap<Int, String>()
+    private val damageStorage = ConcurrentHashMap<String, ConcurrentHashMap<String, MutableSet<ParsedDamagePacket>>>()
+    private val nicknameStorage = ConcurrentHashMap<Int, String>()
 
     fun appendDamage(pdp: ParsedDamagePacket) {
-        damageStorage.getOrPut("${pdp.getActorId()}") { hashMapOf() }
-            .getOrPut("${pdp.getTargetId()}") { hashSetOf() }
-            .add(pdp)
+        val targetMap = damageStorage.computeIfAbsent("${pdp.getActorId()}") { ConcurrentHashMap() }
+        val damageSet = targetMap.computeIfAbsent("${pdp.getTargetId()}") { hashSetOf() }
+
+        synchronized(damageSet) {
+            damageSet.add(pdp)
+        }
     }
 
     fun appendNickname(uid: Int, nickname: String) {
         if (nicknameStorage[uid] != null && nicknameStorage[uid] == nickname) return
+        nicknameStorage[uid] = nickname
         println("$uid 할당 닉네임 변경됨 이전: ${nicknameStorage[uid]} 현재: $nickname")
     }
 
